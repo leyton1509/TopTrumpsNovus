ad46ea0941d115a03e18fc38724cc3a815bbd8dc
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-25 17:15:40 +0000

    Added a columns mapping class

---
+++ TopTrumpsNovus/Models/ColumnNames.cs
@@ -0,0 +1,42 @@
+﻿namespace TopTrumpsNovus.Models
+{
+    public class ColumnNames
+    {
+
+        string[] getColumnNames(int deckID)
+        {
+            if (deckID == 1)
+            {
+                return new string[] { "Physical", "Speed", "Passing", "Defence", "Stamina", "Shot" };
+            }
+            else if (deckID == 2)
+            {
+                return new string[] { "Physical", "Speed", "Passing", "Defence", "Stamina", "Shot" };
+            }
+            else if (deckID == 3)
+            {
+                return new string[] { "Power", "Physical Strength", "Intelligence", "Speed", "Popularity", "Weapons" };
+            }
+            else if (deckID == 4)
+            {
+                return new string[] { "Strength", "Intelligence", "Speed", "Durability", "Plot Armour", "Hax" };
+            }
+            else if (deckID == 5)
+            {
+                return new string[] { "The Force", "Wisdom", "Intelligence", "Diplomacy", "Fighting", "Flying" };
+            }
+            else
+            {
+                return new string[] { };
+            }
+        }
+
+
+
+        }
+
+
+]
+
+    }
+}
