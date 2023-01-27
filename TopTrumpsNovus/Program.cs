8c0127bf7f2e31ddc847c872f757524f9f06ba74
leyton1509 <taylor92@lancaster.ac.uk>
2023-01-27 16:18:28 +0000

    Default changed from home to basketball

--- TopTrumpsNovus/Program.cs
+++ TopTrumpsNovus/Program.cs
@@ -49,7 +49,7 @@ app.UseAuthorization();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+    pattern: "{controller=Card}/{action=BasketBall}/{id?}");
 
 /*
 string connetionString;
